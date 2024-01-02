ad821aee9824bb3f5a6a4fecc0a168c7b02dde9d
Krzysztof Kurowski <228528@student.uek.krakow.pl>
2024-01-02 15:47:20 +0100

    replace buttonColumn for textColumn. Add estimated remaining time

--- ZaczytywanieKodow/Views/ListaKodow.Designer.cs
+++ ZaczytywanieKodow/Views/ListaKodow.Designer.cs
@@ -34,13 +34,15 @@ namespace ZaczytywanieKodow
             this.nazwaPlikuTextBox = new System.Windows.Forms.TextBox();
             this.ZaczytajButton = new System.Windows.Forms.Button();
             this.kodyLista = new System.Windows.Forms.DataGridView();
-            this.kodSystem = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.kodSystem = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.kodDostawcy = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.kodOem = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dostawca = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.wyszukiwania = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.polaczoneNumery = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.smoothProgressBar1 = new SmoothProgressBar.SmoothProgressBar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.kodyLista)).BeginInit();
             this.SuspendLayout();
             // 
@@ -80,52 +82,54 @@ namespace ZaczytywanieKodow
             // kodyLista
             // 
             this.kodyLista.AllowUserToAddRows = false;
+            this.kodyLista.RowHeadersVisible = false;
             this.kodyLista.AllowUserToDeleteRows = false;
             this.kodyLista.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.kodyLista.Location = new System.Drawing.Point(12, 61);
+            this.kodyLista.Location = new System.Drawing.Point(12, 87);
             this.kodyLista.Name = "kodyLista";
             this.kodyLista.RowTemplate.Height = 25;
-            this.kodyLista.Size = new System.Drawing.Size(1169, 874);
+            this.kodyLista.Size = new System.Drawing.Size(1406, 848);
             this.kodyLista.TabIndex = 3;
-            this.kodyLista.CellClick += new DataGridViewCellEventHandler(this.kodyLista_CellClick);
+            this.kodyLista.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.kodyLista_CellClick);
             // 
             // kodSystem
             // 
-            this.kodSystem.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
+            this.kodSystem.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
             this.kodSystem.HeaderText = "Kod Systemowy";
             this.kodSystem.Name = "kodSystem";
+            this.kodSystem.ReadOnly = true;
             // 
             // kodDostawcy
             // 
-            this.kodDostawcy.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
+            this.kodDostawcy.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
             this.kodDostawcy.HeaderText = "Kod Dostawcy";
             this.kodDostawcy.Name = "kodDostawcy";
             this.kodDostawcy.ReadOnly = true;
             // 
             // kodOem
             // 
-            this.kodOem.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.kodOem.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
             this.kodOem.HeaderText = "OEM";
             this.kodOem.Name = "kodOem";
             this.kodOem.ReadOnly = true;
             // 
             // dostawca
             // 
-            this.dostawca.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
+            this.dostawca.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
             this.dostawca.HeaderText = "Dostawca";
             this.dostawca.Name = "dostawca";
             this.dostawca.ReadOnly = true;
             // 
             // wyszukiwania
             // 
-            this.wyszukiwania.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
+            this.wyszukiwania.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
             this.wyszukiwania.HeaderText = "Wyszukiwania";
             this.wyszukiwania.Name = "wyszukiwania";
             this.wyszukiwania.ReadOnly = true;
             // 
             // polaczoneNumery
             // 
-            this.polaczoneNumery.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
+            this.polaczoneNumery.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
             this.polaczoneNumery.HeaderText = "Połączone Numery";
             this.polaczoneNumery.Name = "polaczoneNumery";
             this.polaczoneNumery.ReadOnly = true;
@@ -133,6 +137,8 @@ namespace ZaczytywanieKodow
             // smoothProgressBar1
             // 
             this.smoothProgressBar1.Location = new System.Drawing.Point(674, 11);
+            this.smoothProgressBar1.Maximum = 100;
+            this.smoothProgressBar1.Minimum = 0;
             this.smoothProgressBar1.Name = "smoothProgressBar1";
             this.smoothProgressBar1.ProgressBarColor = System.Drawing.Color.Lime;
             this.smoothProgressBar1.Size = new System.Drawing.Size(507, 25);
@@ -140,12 +146,37 @@ namespace ZaczytywanieKodow
             this.smoothProgressBar1.Value = 0;
             this.smoothProgressBar1.Visible = false;
             // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox1.Location = new System.Drawing.Point(1189, 11);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(167, 24);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "Pozostało czasu:";
+            this.textBox1.Visible = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox2.Location = new System.Drawing.Point(1358, 13);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(63, 24);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.Visible = false;
+            // 
             // ListaKodow
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
             this.AutoSize = true;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.ClientSize = new System.Drawing.Size(1664, 947);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.smoothProgressBar1);
             this.Controls.Add(this.kodyLista);
             this.Controls.Add(this.ZaczytajButton);
@@ -167,12 +198,14 @@ namespace ZaczytywanieKodow
         private TextBox nazwaPlikuTextBox;
         private Button ZaczytajButton;
         private DataGridView kodyLista;
-        private DataGridViewButtonColumn kodSystem;
+        private DataGridViewTextBoxColumn kodSystem;
         private DataGridViewTextBoxColumn kodDostawcy;
         private DataGridViewTextBoxColumn kodOem;
         private DataGridViewTextBoxColumn dostawca;
         private DataGridViewTextBoxColumn wyszukiwania;
         private DataGridViewTextBoxColumn polaczoneNumery;
         private SmoothProgressBar.SmoothProgressBar smoothProgressBar1;
+        private TextBox textBox1;
+        private TextBox textBox2;
     }
 }
--- ZaczytywanieKodow/Views/ListaKodow.cs
+++ ZaczytywanieKodow/Views/ListaKodow.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using ZaczytywanieKodow.Models;
 using System.Linq;
+using System.Configuration;
 
 namespace ZaczytywanieKodow
 {
@@ -27,12 +28,12 @@ namespace ZaczytywanieKodow
             plikExcel.CheckPathExists = true;
             plikExcel.ShowDialog();
             nazwaPlikuTextBox.Text = plikExcel.FileName;
+            if (nazwaPlikuTextBox.Text != "") { ZaczytajButton.BackColor = System.Drawing.Color.Lime; }
         }
 
         private void ZaczytajButton_Click(object sender, EventArgs e)
         {
             items = new List<Item>();
-            ZaczytajButton.BackColor = System.Drawing.Color.Lime;
             kodyLista.Columns.AddRange(new DataGridViewColumn[] { kodSystem, kodDostawcy, kodOem, dostawca, wyszukiwania, polaczoneNumery });
             Excel plik = new Excel(nazwaPlikuTextBox.Text);
             plik.Otworz();
@@ -42,17 +43,35 @@ namespace ZaczytywanieKodow
             this.smoothProgressBar1.Minimum = 0;
             this.smoothProgressBar1.Maximum = plik.PobierzIloscWierszy();
 
+            Stopwatch sw;
+            long totalTime = 0;
+            int secondsLeft;
+            int minutesLeft;
+            string formattedTime;
+
             for (int i = 2; i <= plik.PobierzIloscWierszy() + 2; i++)
             {
+                sw = Stopwatch.StartNew();
                 Item item = plik.Czytaj(i);
+
                 if (item.KodOem == "") { break; } 
                 item.Id = i - 2;
                 item.PolaczoneKody = "";
                 items.Add(item);
-                if (i % 5 == 0) { Application.DoEvents(); }
-                this.smoothProgressBar1.Value++;
 
+                this.smoothProgressBar1.Value++;
+                totalTime = totalTime + sw.ElapsedMilliseconds;
+                secondsLeft = (int)Math.Floor((double)totalTime / i / 1000 * (plik.PobierzIloscWierszy() + 2 - i));
+                minutesLeft = (int)Math.Floor((decimal)secondsLeft / 60);
+                formattedTime = (secondsLeft - (minutesLeft * 60)) < 10 ? minutesLeft.ToString() + ":0" + (secondsLeft - (minutesLeft * 60)).ToString() : minutesLeft.ToString() + ":" + (secondsLeft - (minutesLeft * 60)).ToString();
+
+                textBox1.Visible = true;
+                textBox2.Visible = true;
+                textBox2.Text = formattedTime;
+
+                if (i % 3 == 0) { Application.DoEvents(); }
             }
+
             foreach (var item in items)
             {
                 if (item.KodSystem.Count == 1)
@@ -62,8 +81,12 @@ namespace ZaczytywanieKodow
                 else
                 {
                     kodyLista.Rows.Add("Wybierz", item.KodDostawcy, item.KodOem, "", item.Wyszukiwania, item.PolaczoneKody);
+                    kodyLista.Rows[item.Id].Cells["kodSystem"].Style.BackColor = System.Drawing.Color.Red;
                 }
             }
+            this.smoothProgressBar1.Value = this.smoothProgressBar1.Maximum;
+            timeLeft = "0:00";
+            textBox2.Text = timeLeft;
         }
 
         private void kodyLista_CellClick(object sender, DataGridViewCellEventArgs e)
