4acee207684290926e71c14c854d94768e5737b0
Krzysztof Kurowski <228528@student.uek.krakow.pl>
2024-01-11 09:38:29 +0100

    add new form

---
+++ ZaczytywanieKodow/Views/Szczegoly.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ZaczytywanieKodow.Views
+{
+    public partial class Szczegoly : Form
+    {
+        private int twrGidNumer { get; set; }
+        public Szczegoly(int twrGidNumer)
+        {
+            this.twrGidNumer = twrGidNumer;
+            InitializeComponent();
+            dataGridView1.DataSource = Excel.Szczegoly(twrGidNumer);
+        }
+    }
+}
